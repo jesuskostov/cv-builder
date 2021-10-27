fb2a75d5e1246757e3f9785838fd28615588f3df
jesuskostov <jesuskostov@gmail.com>
2021-10-27 09:31:33 +0300

    gtag try

--- src/main.js
+++ src/main.js
@@ -10,9 +10,13 @@ import "../node_modules/vue-multiselect/dist/vue-multiselect.min.css";
 import VueAwesomeSwiper from "vue-awesome-swiper";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import VueGtag from "vue-gtag";
 
 Vue.config.productionTip = false;
 
+Vue.use(VueGtag, {
+  config: { id: process.env.VUE_APP_GOOGLE_ANALITICS_ID }
+});
 Vue.use(VueAwesomeSwiper);
 Vue.use(VeeValidate);
 Vue.use(BootstrapVue);
