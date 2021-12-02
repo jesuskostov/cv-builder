fd07f5549b1790e8f11753281053eef3b40217f2
root <root@wp2.nori.adm.hosti.la>
2021-12-02 16:40:00 +0100

    changed public dist and so on

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,23 @@
 const path = require("path");
 
 module.exports = {
-    outputDir: path.resolve(__dirname, "../public/"),
+    outputDir: path.resolve(__dirname, "./public/"),
+    chainWebpack: config => {
+      config
+        .plugin('html')
+        .tap(args => {
+          args[0].template = './src-public/index.html'
+          return args
+        })
+
+        
+    },
     configureWebpack: {
       devServer: {
         historyApiFallback: true
       }
     }
-  
   };
+
+  // TODO: copy plugin
+  // https://forum.vuejs.org/t/vue-cli3-how-to-change-default-public-directory/38796/3
