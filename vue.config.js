6a7e5337bfc78459bdaca4910ccf943d134c705d
jesuskostov <jesuskostov@gmail.com>
2021-12-02 17:17:33 +0200

    public

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
-module.exports = {
+const path = require("path");
 
+module.exports = {
+    outputDir: path.resolve(__dirname, "../public/"),
     configureWebpack: {
       devServer: {
         historyApiFallback: true
